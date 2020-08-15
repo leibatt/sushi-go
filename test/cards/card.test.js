c6fce9966e74987cb95d545c7946f1a125694267
Leilani Battle <leibatt@.mit.edu>
2020-08-15 12:56:16 -0400

    removed some old code. should have sufficient unit test coverage for util and cards.Card

--- test/cards/card.test.js
+++ test/cards/card.test.js
@@ -1,284 +1,204 @@
-const assert = require("assert");
-const cards = require("../cards")
+const assert = require('chai').assert;
+const cards = require("../../cards")
 
 runTests = function() {
   describe("Card", function() {
-    describe("#sumStack()", function() {
-      it("empty stacks should return 0", function() {
-        assert.equal(game_objects.Card.sumStack([]), 0);
+    describe("#isRelevantStack()", function() {
+      it("cards of all the same type should be relevant", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        card.type = "test";
+        card2.type = "test";
+        card3.type = "test";
+        assert.isOk(card.isRelevantStack([card2,card3]));
+        assert.isOk(card2.isRelevantStack([card,card3]));
+        assert.isOk(card3.isRelevantStack([card,card2]));
       });
-
-      it("stacks of one card should return the card's value", function() {
-        var card = new game_objects.Card(4);
-        assert.equal(game_objects.Card.sumStack([card]), 4);
-      });
-
-      it("stacks of 2 cards (examples: (1,5) and (2,4)) should return the sum of the 2 (6)", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(5);
-        assert.equal(game_objects.Card.sumStack([card,card2]),6);
-        card = new game_objects.Card(2);
-        card2 = new game_objects.Card(4);
-        assert.equal(game_objects.Card.sumStack([card,card2]),6);
-      });
-
-      it("sum of 5 cards (1, 2, 3, 4, 5) should return 15", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var card4 = new game_objects.Card(4);
-        var card5 = new game_objects.Card(5);
-        assert.equal(game_objects.Card.sumStack([card,card2,card3,card4,card5]),15);
+      it("cards with different types should not be relevant", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        card.type = "test";
+        card2.type = "test";
+        card3.type = "something";
+        assert.isNotOk(card.isRelevantStack([card2,card3]));
+        assert.isNotOk(card2.isRelevantStack([card,card3]));
+        assert.isNotOk(card3.isRelevantStack([card,card2]));
       });
     });
-
+    describe("#isValidStack()", function() {
+      it("return the same results as isRelevantStack", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        card.type = "test";
+        card2.type = "test";
+        card3.type = "test";
+        assert.equal(card.isRelevantStack([card2,card3]),card.isValidStack([card2,card3]));
+        card3.type = "something";
+        assert.equal(card.isRelevantStack([card2,card3]),card.isValidStack([card2,card3]));
+      });
+    });
+    describe("#rankCardsByValue()", function() {
+      it("rank cards with value 1, 2, 3 as 3, 2, 1", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var orig = [card,card2,card3];
+        var target = [card3,card2,card];
+        var ranked = cards.Card.rankCardsByValue(orig.slice()); // pass a copy, just in case
+        for(var i = 0; i < target.length; i++) {
+          assert.equal(target[i].value,ranked[i].value);
+        }
+      });
+    });
     describe("#findMaxValueCard()", function() {
       it("empty stack should return null", function() {
-        var maxCard = game_objects.Card.findMaxValueCard([]);
+        var maxCard = cards.Card.findMaxValueCard([]);
         assert.equal(maxCard, null);
       });
-
       it("stack of 1 card returns the value of that card", function() {
-        var card = new game_objects.Card(1);
-        var maxCard = game_objects.Card.findMaxValueCard([card]);
+        var card = new cards.Card(1);
+        var maxCard = cards.Card.findMaxValueCard([card]);
         assert.equal(maxCard.id, card.id);
       });
-      it("check set of 2", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var maxCard = game_objects.Card.findMaxValueCard([card,card2]);
+      it("stack of 2 cards (1,2) should return the ID of the higher value card (2)", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var maxCard = cards.Card.findMaxValueCard([card,card2]);
         assert.equal(maxCard.id, card2.id);
+      });
+      it("stack of 3 cards (1,2,3) should return the ID of the highest value card (3)", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var maxCard = cards.Card.findMaxValueCard([card,card2,card3]);
+        assert.equal(maxCard.id,card3.id);
       });
     });
     describe("#getMaxPlayerStack()", function() {
       it("check no stacks", function() {
-        assert.equal(game_objects.Card.getMaxPlayerStack([]), null);
+        assert.equal(cards.Card.getMaxPlayerStack([]), null);
       });
       it("check empty stack", function() {
-        assert.equal(game_objects.Card.getMaxPlayerStack([[]]), 0);
+        assert.equal(cards.Card.getMaxPlayerStack([[]]), 0);
       });
       it("check one stack", function() {
-        var card = new game_objects.Card(1);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[card]]), 0);
+        var card = new cards.Card(1);
+        assert.equal(cards.Card.getMaxPlayerStack([[card]]), 0);
       });
       it("check two stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[],[]]), 0);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[card],[card2]]), 1);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[],[card,card2]]), 1);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[card2,card2],[card3]]), 0);
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        assert.equal(cards.Card.getMaxPlayerStack([[],[]]), 0);
+        assert.equal(cards.Card.getMaxPlayerStack([[card],[card2]]), 1);
+        assert.equal(cards.Card.getMaxPlayerStack([[],[card,card2]]), 1);
+        assert.equal(cards.Card.getMaxPlayerStack([[card2,card2],[card3]]), 0);
       });
       it("check three stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[],[],[]]), 0);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[card3],[card2],[card]]), 0);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[card],[card3],[card2]]), 1);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[card],[card2],[card3]]), 2);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[],[card],[card2,card3]]), 2);
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        assert.equal(cards.Card.getMaxPlayerStack([[],[],[]]), 0);
+        assert.equal(cards.Card.getMaxPlayerStack([[card3],[card2],[card]]), 0);
+        assert.equal(cards.Card.getMaxPlayerStack([[card],[card3],[card2]]), 1);
+        assert.equal(cards.Card.getMaxPlayerStack([[card],[card2],[card3]]), 2);
+        assert.equal(cards.Card.getMaxPlayerStack([[],[card],[card2,card3]]), 2);
       });
       it("check five stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var card4 = new game_objects.Card(4);
-        var card5 = new game_objects.Card(5);
-        assert.equal(game_objects.Card.getMaxPlayerStack([[],[card],[card,card2,card3],[card4,card5],[]]), 3);
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var card4 = new cards.Card(4);
+        var card5 = new cards.Card(5);
+        assert.equal(cards.Card.getMaxPlayerStack([[],[card],[card,card2,card3],[card4,card5],[]]), 3);
       });
     });
     describe("#rankPlayerStacks()", function() {
       it("check no stacks", function() {
-        assert.equal(game_objects.Card.rankPlayerStacks([]).length, 0);
+        assert.equal(cards.Card.rankPlayerStacks([]).length, 0);
       });
       it("check empty stack", function() {
-        var rankings = game_objects.Card.rankPlayerStacks([[]]); 
+        var rankings = cards.Card.rankPlayerStacks([[]]); 
         assert.equal(rankings[0].idx, 0);
       });
       it("check one stack", function() {
-        var card = new game_objects.Card(1);
-        var rankings = game_objects.Card.rankPlayerStacks([[card]]); 
+        var card = new cards.Card(1);
+        var rankings = cards.Card.rankPlayerStacks([[card]]); 
         assert.equal(rankings[0].idx, 0);
       });
       it("check two stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var rankings = game_objects.Card.rankPlayerStacks([[card],[card2]]); 
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var rankings = cards.Card.rankPlayerStacks([[card],[card2]]); 
         assert.equal(rankings[0].idx, 1);
         assert.equal(rankings[1].idx, 0);
-        rankings = game_objects.Card.rankPlayerStacks([[],[card,card2]]);
+        rankings = cards.Card.rankPlayerStacks([[],[card,card2]]);
         assert.equal(rankings[0].idx, 1);
         assert.equal(rankings[1].idx, 0);
-        rankings = game_objects.Card.rankPlayerStacks([[card2,card2],[card3]]);
+        rankings = cards.Card.rankPlayerStacks([[card2,card2],[card3]]);
         assert.equal(rankings[0].idx, 0);
         assert.equal(rankings[1].idx, 1);
       });
       it("check three stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var rankings = game_objects.Card.rankPlayerStacks([[card3],[card2],[card]]); 
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var rankings = cards.Card.rankPlayerStacks([[card3],[card2],[card]]); 
         assert.equal(rankings[0].idx, 0);
         assert.equal(rankings[1].idx, 1);
         assert.equal(rankings[2].idx, 2);
-        rankings = game_objects.Card.rankPlayerStacks([[card],[card3],[card2]]);
+        rankings = cards.Card.rankPlayerStacks([[card],[card3],[card2]]);
         assert.equal(rankings[0].idx, 1);
         assert.equal(rankings[1].idx, 2);
         assert.equal(rankings[2].idx, 0);
-        rankings = game_objects.Card.rankPlayerStacks([[card],[card2],[card3]]);
+        rankings = cards.Card.rankPlayerStacks([[card],[card2],[card3]]);
         assert.equal(rankings[0].idx, 2);
         assert.equal(rankings[1].idx, 1);
         assert.equal(rankings[2].idx, 0);
-        rankings = game_objects.Card.rankPlayerStacks([[card],[],[card2,card3]]);
+        rankings = cards.Card.rankPlayerStacks([[card],[],[card2,card3]]);
         assert.equal(rankings[0].idx, 2);
         assert.equal(rankings[1].idx, 0);
         assert.equal(rankings[2].idx, 1);
       });
       it("check five stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var card4 = new game_objects.Card(4);
-        var card5 = new game_objects.Card(5);
-        var rankings = game_objects.Card.rankPlayerStacks([[],[card],[card,card2,card3],[card4,card5],[]]); 
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var card4 = new cards.Card(4);
+        var card5 = new cards.Card(5);
+        var rankings = cards.Card.rankPlayerStacks([[],[card],[card,card2,card3],[card4,card5],[]]); 
         assert.equal(rankings[0].idx,3);
         assert.equal(rankings[1].idx,2);
         assert.equal(rankings[2].idx,1);
       });
     });
-    describe("#scoreStacksByRank()", function() {
-      it("check no stacks", function() {
-        assert.equal(game_objects.Card.scoreStacksByRank([]).length, 0);
+    describe("#sumStack()", function() {
+      it("empty stacks should return 0", function() {
+        assert.equal(cards.Card.sumStack([]), 0);
       });
-      it("check empty stack", function() {
-        var scores = game_objects.Card.scoreStacksByRank([[]]); 
-        assert.equal(scores[0], 0);
+      it("stacks of one card should return the card's value", function() {
+        var card = new cards.Card(4);
+        assert.equal(cards.Card.sumStack([card]), 4);
       });
-      it("check one stack", function() {
-        var card = new game_objects.Card(1);
-        var scores = game_objects.Card.scoreStacksByRank([[card]]); 
-        assert.equal(scores[0], 0);
+      it("stacks of 2 cards (examples: (1,5) and (2,4)) should return the sum of the 2 cards (6)", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(5);
+        assert.equal(cards.Card.sumStack([card,card2]),6);
+        card = new cards.Card(2);
+        card2 = new cards.Card(4);
+        assert.equal(cards.Card.sumStack([card,card2]),6);
       });
-      it("check two stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        scores = game_objects.Card.scoreStacksByRank([[card],[card2]]); 
-        assert.equal(scores[0], 3);
-        assert.equal(scores[1], 6);
-        scores = game_objects.Card.scoreStacksByRank([[],[card,card2]]);
-        assert.equal(scores[0], 3);
-        assert.equal(scores[1], 6);
-        scores = game_objects.Card.scoreStacksByRank([[card2,card2],[card3]]);
-        assert.equal(scores[0], 6);
-        assert.equal(scores[1], 3);
-      });
-      it("check three stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var scores = game_objects.Card.scoreStacksByRank([[card3],[card2],[card]]); 
-        assert.equal(scores[0], 6);
-        assert.equal(scores[1], 3);
-        assert.equal(scores[2], 0);
-        scores = game_objects.Card.scoreStacksByRank([[card],[card3],[card2]]);
-        assert.equal(scores[0], 0);
-        assert.equal(scores[1], 6);
-        assert.equal(scores[2], 3);
-        scores = game_objects.Card.scoreStacksByRank([[card],[card2],[card3]]);
-        assert.equal(scores[0], 0);
-        assert.equal(scores[1], 3);
-        assert.equal(scores[2], 6);
-        scores = game_objects.Card.scoreStacksByRank([[card],[],[card2,card3]]);
-        assert.equal(scores[0], 3);
-        assert.equal(scores[1], 0);
-        assert.equal(scores[2], 6);
-      });
-      it("check five stacks", function() {
-        var card = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        var card4 = new game_objects.Card(4);
-        var card5 = new game_objects.Card(5);
-        var scores = game_objects.Card.scoreStacksByRank([[],[card],[card,card2,card3],[card4,card5],[]]); 
-        assert.equal(scores[0],0);
-        assert.equal(scores[1],0);
-        assert.equal(scores[2],3);
-        assert.equal(scores[3],6);
-        assert.equal(scores[4],0);
-      });
-      it("check all scores equal", function() {
-        var card2 = new game_objects.Card(2);
-        for(var i = 3; i <= 5; i++) { // for each number of players
-          var stacks = [];
-          while(stacks.length < i) {
-            stacks.push([card2]);
-          }
-          var scores = game_objects.Card.scoreStacksByRank(stacks); 
-          for(var j = 0; j < scores.length; j++) {
-            assert.equal(scores[j],0);
-          }
-        }
-      });
-      it("check first place ties", function() {
-        var card1 = new game_objects.Card(1);
-        var card2 = new game_objects.Card(2);
-        for(var i = 3; i <= 5; i++) { // for each number of players
-          var stacks = [[card1]];
-          while(stacks.length < i) {
-            stacks.push([card2]);
-          }
-          var scores = game_objects.Card.scoreStacksByRank(stacks); 
-          var expectedScore = Math.floor(6/(i-1));
-          for(var j = 0; j < scores.length; j++) {
-            if(j === 0) {
-              assert.equal(scores[j],0);
-            } else {
-              assert.equal(scores[j],expectedScore);
-            }
-          }
-        }
-      });
-      it("check second place ties", function() {
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        for(var i = 2; i <= 5; i++) { // for each number of players
-          var stacks = [[card3]];
-          while(stacks.length < i) {
-            stacks.push([card2]);
-          }
-          var scores = game_objects.Card.scoreStacksByRank(stacks); 
-          var expectedScore = Math.floor(3/(i-1));
-          for(var j = 0; j < scores.length; j++) {
-            if(j === 0) {
-              assert.equal(scores[j],6);
-            } else {
-              assert.equal(scores[j],expectedScore);
-            }
-          }
-        }
-      });
-      it("check last place penalty, no second place", function() {
-        var card2 = new game_objects.Card(2);
-        var card3 = new game_objects.Card(3);
-        for(var i = 2; i <= 5; i++) { // for each number of players
-          var stacks = [[card3]];
-          while(stacks.length < i) {
-            stacks.push([card2]);
-          }
-          var scores = game_objects.Card.scoreStacksByRank(stacks,secondPlace=false,penalizeMin=true); 
-          var expectedScore = Math.ceil(-6.0/(i-1));
-          for(var j = 0; j < scores.length; j++) {
-            if(j === 0) {
-              assert.equal(scores[j],6);
-            } else {
-              assert.equal(scores[j],expectedScore);
-            }
-          }
-        }
+      it("sum of 5 cards (1, 2, 3, 4, 5) should return 15", function() {
+        var card = new cards.Card(1);
+        var card2 = new cards.Card(2);
+        var card3 = new cards.Card(3);
+        var card4 = new cards.Card(4);
+        var card5 = new cards.Card(5);
+        assert.equal(cards.Card.sumStack([card,card2,card3,card4,card5]),15);
       });
     });
   });
--- test/util.test.js
+++ test/util.test.js
@@ -1,14 +1,13 @@
-var assert = require('chai').assert;
-//const assert = require('assert');
+const assert = require('chai').assert;
 const util = require('../util');
 
 
 describe('testing util functions', function() {
   describe("#randomIntegerFromRange()", function() {
-    it("should produce numbers between 7 and 9", function() {
+    it("should produce numbers between -7 and 9", function() {
       for(var i = 0; i < 10; i++) {
         var result = util.randomIntegerFromRange(7,9);
-        assert.isOk(result >= 7 && result <= 9);
+        assert.isOk(result >= -7 && result <= 9);
       }
     });
     it("should produce numbers between 6 and 51", function() {
@@ -17,10 +16,16 @@ describe('testing util functions', function() {
         assert.isOk(result >= 6 && result <= 51);
       }
     });
-    it("should produce numbers between 13 and 27", function() {
+    it("should produce numbers between 0 and 1", function() {
       for(var i = 0; i < 10; i++) {
-        var result = util.randomIntegerFromRange(13,27);
-        assert.isOk(result >= 13 && result <= 27);
+        var result = util.randomIntegerFromRange(0,1);
+        assert.isOk(result >= 0 && result <= 1);
+      }
+    });
+    it("should produce numbers between 83,111 and 2,706,318", function() {
+      for(var i = 0; i < 10; i++) {
+        var result = util.randomIntegerFromRange(83111,2706318);
+        assert.isOk(result >= 83111 && result <= 2706318);
       }
     });
   });
@@ -43,6 +48,33 @@ describe('testing util functions', function() {
         var result = util.randomInteger(1954);
         assert.isOk(result >= 0 && result <= 1954);
       }
+    });
+  });
+  describe("#sum()", function() {
+    it("should sum [1, 2, 3, 4] as 10", function() {
+      assert.equal(util.sum([1,2,3,4]),10);
+    });
+    it("should sum [-1, 1]  as 0", function() {
+      assert.equal(util.sum([-1,1]),0);
+    });
+    it("should sum []  as 0", function() {
+      assert.equal(util.sum([]),0);
+    });
+  });
+  describe("#shuffle()", function() {
+    it("should still contain [1, 2, 3, 4] in shuffled array", function() {
+      var res = [1,2,3,4];
+      util.shuffle(res);
+      [1,2,3,4].forEach(i => assert.isOk(res.indexOf(i) >= 0));
+    });
+    it("should still contain [-1, 0, 1] in shuffled array", function() {
+      var res = [-1,0,1];
+      util.shuffle(res);
+      [-1,0,1].forEach(i => assert.isOk(res.indexOf(i) >= 0));
+    });
+    it("[] should still be empty after shuffle", function() {
+      var res = [];
+      assert.isOk(res.length === 0);
     });
   });
 });
